bd5a5ef4f6b39380dc95e608a51d3d8b5654b527
Jon Rexeisen <jrexeise@nerdery.com>
2011-11-03 14:56:26 +0000

    Added networking controls
    git-svn-id: https://svn.sierrabravo.net/BRAVO.ProductDevelopment/JSONModeler/trunk@5 ea3e3d88-1103-4116-9a39-8646a12f7446

---
+++ JSONModeler/JSONModeler/ModelController/JSONFetcher.h
@@ -0,0 +1,13 @@
+//
+//  JSONFetcher.h
+//  JSONModeler
+//
+//  Created by Jon Rexeisen on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONFetcher : NSObject
+
+@end
---
+++ JSONModeler/JSONModeler/ModelController/JSONFetcher.m
@@ -0,0 +1,13 @@
+//
+//  JSONFetcher.m
+//  JSONModeler
+//
+//  Created by Jon Rexeisen on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JSONFetcher.h"
+
+@implementation JSONFetcher
+
+@end
